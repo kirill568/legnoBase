82442866826e41b166c756a9d4c27f7e9ba37f9f
kirill568 <kirill568@gmail.com>
2019-10-30 12:26:45 +0700

    fixOrdering

--- base/models.py
+++ base/models.py
@@ -15,8 +15,14 @@ class Builders(models.Model):
 	def __str__(self):
 		return self.name
 
+	class Meta:
+		ordering = ['id']
+
 class Finishers(models.Model):
 	name = models.CharField(max_length = 255, unique = True)
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		ordering = ['id']
